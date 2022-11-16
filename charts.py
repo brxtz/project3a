7fe38b1518eb98cad0f50f070f62553d4067face
Rafael Bratfich <90302625+brxtz@users.noreply.github.com>
2022-11-15 18:45:43 -0600

    Add files via upload

--- charts.py
+++ charts.py
@@ -1,6 +1,7 @@
 '''
 This web service extends the Alphavantage api by creating a visualization module, 
 converting json query results retuned from the api into charts and other graphics. 
+
 This is where you should add your code to function query the api
 '''
 import requests
@@ -8,6 +9,10 @@ from datetime import datetime
 from datetime import date
 import pygal
 
+
+#Helper function for converting date
+def convert_date(str_date):
+    return datetime.strptime(str_date, '%Y-%m-%d').date()
 
 def Get_Stock_API(StockSymbol,TimeSeries):
 
@@ -27,10 +32,6 @@ def Get_Stock_API(StockSymbol,TimeSeries):
     r = requests.get(url).json()
     return r
 
-#Helper function for converting date
-def convert_date(str_date):
-    return datetime.strptime(str_date, '%Y-%m-%d').date()
-
 def GenerateChart(symbol,chart_type,time_series,start_date,end_date):
     data = Get_Stock_API(symbol,time_series)
 
@@ -48,64 +49,31 @@ def GenerateChart(symbol,chart_type,time_series,start_date,end_date):
                 HighList.append(float(data['Time Series (30min)'][i]['2. high']))
                 LowList.append(float(data['Time Series (30min)'][i]['3. low']))
                 CloseList.append(float(data['Time Series (30min)'][i]['4. close']))
-                
-        '''
-        elif(TimeSeries == "TIME_SERIES_DAILY"):
-            for i in data['Time Series (Daily)']:
-                DateFilter.append(i.split(' ')[0])
-                year = (int(DateFilter[count].split('-')[0]))
-                month =(int(DateFilter[count].split('-')[1]))
-                day =(int(DateFilter[count].split('-')[2]))
-                count+=1
-                ParamStart = datetime.date(int(dateparams[1][0]),int(dateparams[1][1]),int(dateparams[1][2]))
-                ParamEnd = datetime.date(int(dateparams[2][0]),int(dateparams[2][1]),int(dateparams[2][2]))
-                CurrentDate =datetime.date(year,month,day)
-                if((CurrentDate>=ParamStart)&(CurrentDate<=ParamEnd)):
-                    Timelist.append(i)
-                    TimeDateList.append(i.split(' ')[0])
-            for t in Timelist:
-                OpenList.append(float(data['Time Series (Daily)'][t]['1. open']))
-                HighList.append(float(data['Time Series (Daily)'][t]['2. high']))
-                LowList.append(float(data['Time Series (Daily)'][t]['3. low']))
-                CloseList.append(float(data['Time Series (Daily)'][t]['4. close']))
-        elif(TimeSeries == "TIME_SERIES_WEEKLY"):
-            for i in data['Weekly Time Series']:
-                DateFilter.append(i.split(' ')[0])
-                year = (int(DateFilter[count].split('-')[0]))
-                month =(int(DateFilter[count].split('-')[1]))
-                day =(int(DateFilter[count].split('-')[2]))
-                count+=1
-                ParamStart = datetime.date(int(dateparams[1][0]),int(dateparams[1][1]),int(dateparams[1][2]))
-                ParamEnd = datetime.date(int(dateparams[2][0]),int(dateparams[2][1]),int(dateparams[2][2]))
-                CurrentDate =datetime.date(year,month,day)
-                if((CurrentDate>=ParamStart)&(CurrentDate<=ParamEnd)):
-                    Timelist.append(i)
-                    TimeDateList.append(i.split(' ')[0])
-            for t in Timelist:
-                OpenList.append(float(data['Weekly Time Series'][t]['1. open']))
-                HighList.append(float(data['Weekly Time Series'][t]['2. high']))
-                LowList.append(float(data['Weekly Time Series'][t]['3. low']))
-                CloseList.append(float(data['Weekly Time Series'][t]['4. close']))
-        else:
-            for i in data['Monthly Time Series']:
-                DateFilter.append(i.split(' ')[0])
-                year = (int(DateFilter[count].split('-')[0]))
-                month =(int(DateFilter[count].split('-')[1]))
-                day =(int(DateFilter[count].split('-')[2]))
-                count+=1
-                ParamStart = datetime.date(int(dateparams[1][0]),int(dateparams[1][1]),int(dateparams[1][2]))
-                ParamEnd = datetime.date(int(dateparams[2][0]),int(dateparams[2][1]),int(dateparams[2][2]))
-                CurrentDate =datetime.date(year,month,day)
-                if((CurrentDate>=ParamStart)&(CurrentDate<=ParamEnd)):
-                    Timelist.append(i)
-                    TimeDateList.append(i.split(' ')[0])
-            for t in Timelist:
-                OpenList.append(float(data['Monthly Time Series'][t]['1. open']))
-                HighList.append(float(data['Monthly Time Series'][t]['2. high']))
-                LowList.append(float(data['Monthly Time Series'][t]['3. low']))
-                CloseList.append(float(data['Monthly Time Series'][t]['4. close']))
-                '''        
-
+    elif(time_series == '2'): 
+        for i in data['Time Series (Daily)']:
+            current_date = convert_date(i[:10])
+            if((current_date>=start_date)&(current_date<=end_date)):
+                OpenList.append(float(data['Time Series (Daily)'][i]['1. open']))
+                HighList.append(float(data['Time Series (Daily)'][i]['2. high']))
+                LowList.append(float(data['Time Series (Daily)'][i]['3. low']))
+                CloseList.append(float(data['Time Series (Daily)'][i]['4. close']))
+    elif(time_series == '3'): 
+        for i in data['Weekly Time Series']:
+            current_date = convert_date(i[:10])
+            if((current_date>=start_date)&(current_date<=end_date)):
+                OpenList.append(float(data['Weekly Time Series'][i]['1. open']))
+                HighList.append(float(data['Weekly Time Series'][i]['2. high']))
+                LowList.append(float(data['Weekly Time Series'][i]['3. low']))
+                CloseList.append(float(data['Weekly Time Series'][i]['4. close']))
+    else:
+        for i in data['Monthly Time Series']:
+            current_date = convert_date(i[:10])
+            if((current_date>=start_date)&(current_date<=end_date)):
+                OpenList.append(float(data['Monthly Time Series'][i]['1. open']))
+                HighList.append(float(data['Monthly Time Series'][i]['2. high']))
+                LowList.append(float(data['Monthly Time Series'][i]['3. low']))
+                CloseList.append(float(data['Monthly Time Series'][i]['4. close']))
+    
     if(chart_type == '2'):
         line_chart = pygal.Line()
         line_chart.title = "Stock Data for " + symbol +": " + start_date.strftime("%Y-%m-%d") + " to " + end_date.strftime("%Y-%m-%d")
